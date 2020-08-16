30983cf51236b8ec65f9509f3e771918f64a48a2
pkhandel92 <pkhandel992@gmail.com>
2020-08-16 16:50:28 +0530

    Added comments and some modifications

--- src/main/java/com/prep/ds/design/ConnectionPool.java
+++ src/main/java/com/prep/ds/design/ConnectionPool.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 public abstract class ConnectionPool<T> {
     //Using a queue to keep tasks available at O(1);
@@ -13,7 +14,9 @@ public abstract class ConnectionPool<T> {
     //Keeping a map entry for each available/unavailable queue to get last executed time of task for ttl
     private Map<T,Long> availiableConnMap;
     private Map<T,Long> inUseConnectionMap;
+    //Total no of connections
     private int corePoolSize;
+    // amount of time in which each connection will expire
     private int expiryTime;
 
     public ConnectionPool(int corePoolSize, int expiryTime) {
@@ -27,23 +30,29 @@ public abstract class ConnectionPool<T> {
     public T getConnection() throws Throwable {
         T t=null;
         long now=System.currentTimeMillis();
-        while(!availableConn.isEmpty()){
-            T temp=availableConn.poll();
+        while(!availableConn.isEmpty()){//iterate through the available connections
+            T temp=availableConn.poll();//fetch first available connection
             if(availiableConnMap.get(temp)!=null){
+                //check time of the available connection
                 long currTime= availiableConnMap.get(temp);
                 if(now-currTime>expiryTime){
+                    //if time expires remove that connection
                     availiableConnMap.remove(temp);
                     availableConn.remove(temp);
+                    //responsibility of kiilliing the connection will be with caller
                     killConnection(temp);
                 }else{
-                    if(isValid(temp)) {
+                    if(isValid(temp)) {//check if connection is still valid**. Responsibility with valid
                          t=temp;
+                         //Remove the connection from available connections
                          availableConn.remove(t);
                          availiableConnMap.remove(t);
+                         //Add to in Use connections
                         inUseConnectionMap.put(t, now);
                         inUseConn.offer(t);
                         return t;
                     }else{
+                        //if connection is not valid remove and kill the connection
                         availiableConnMap.remove(temp);
                         availableConn.remove(temp);
                         killConnection(temp);
@@ -52,10 +61,16 @@ public abstract class ConnectionPool<T> {
                 }
             }
         }
-        if(t==null){
-            t=create();
-            availiableConnMap.put(t,now);
-            availableConn.offer(t);
+        if(t==null){//No need for this check just a fail safe
+            //Create a connection if available connections had no reusable connections
+            if(inUseConn.size()<corePoolSize) {
+                t = create();
+                inUseConnectionMap.put(t, now);
+                inUseConn.offer(t);
+            }
+            else{
+                throw new RejectedExecutionException("No connections available");
+            }
         }
         return t;
     }
